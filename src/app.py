4bced31023e2e012427435e062dd5d84b81e294c
Nicolas Huguenin <nicolas@unit8.co>
2020-11-22 11:44:59 +0100

    feat(webapp): changed route for fizzbuzz resource

--- src/app.py
+++ src/app.py
@@ -4,6 +4,6 @@ from .fizzbuzz import fizzbuzz_1_to_number
 app = Flask(__name__)
 
 
-@app.route('/fizzbuzz/<int:number>', methods=["GET"])
+@app.route('/api/fizzbuzz/<int:number>', methods=["GET"])
 def get_fizzbuzz(number: int):
     return jsonify(fizzbuzz_1_to_number(number))
